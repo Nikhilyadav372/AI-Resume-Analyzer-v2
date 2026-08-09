c1c238ad848ec929c0891f690805d9dbd439bc39
unknown <mr30nikhil@gmail.com>
2026-08-09 16:07:48 +0530

    Fix Railway deployment configuration

--- database.py
+++ database.py
@@ -1,15 +1,21 @@
+import os
 import mysql.connector
 from mysql.connector import Error
 from config import Config
-
 
 def get_connection():
     try:
         connection = mysql.connector.connect(
             host=Config.DB_HOST,
+            port=Config.DB_PORT,
             user=Config.DB_USER,
             password=Config.DB_PASSWORD,
-            database=Config.DB_NAME
+            database=Config.DB_NAME,
+            ssl_ca=os.path.join(
+                os.path.dirname(__file__),
+                "certificates",
+                "ca.pem"
+            )
         )
 
         if connection.is_connected():
@@ -17,9 +23,5 @@ def get_connection():
             return connection
 
     except Error as e:
-        print("❌ Error:", e)
+        print("❌ Database Connection Error:", e)
         return None
-    if connection.is_connected():
-     print("✅ Database Connected Successfully")
-    print("Connected Database:", connection.database)
-    return connection
